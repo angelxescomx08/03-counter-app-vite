fcdfd603ce2cca5d82c1ef435c019a089d36b4fd
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2022-12-18 12:17:06 -0600

    React testing library

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CounterApp } from './CounterApp'
-//import { FirstApp } from './FirstApp'
+//import { CounterApp } from './CounterApp'
+import { FirstApp } from './FirstApp'
 
 //import { HelloWorldApp } from './HelloWorldApp'
 
@@ -9,6 +9,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <CounterApp />
+        {/*<CounterApp />*/}
+        <FirstApp/>
     </React.StrictMode>
 )
