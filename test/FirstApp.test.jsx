e721bd43677024853ce35e34b387e8cc97a31437
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2022-12-22 21:53:16 -0600

    Pruebas para evaluar un contenido especifico

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -4,6 +4,17 @@ import { FirstApp } from '../src/FirstApp'
 describe('Pruebas en <FirstApp />', () => {
     test('Debe hacer un mattch con el snapshot', () => {
         const title = 'Hola angel'
-        render(<FirstApp title={title} />)
+        const { container } = render(<FirstApp title={title} />);
+        expect(container).toMatchSnapshot();
+    })
+
+    test('Debe de mostrar el título en un h1', () => {
+        const title = 'Hola, Soy Goku';
+        const { container, getByText } = render(<FirstApp title={title} />)
+
+        expect(getByText(title)).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
     })
 })
